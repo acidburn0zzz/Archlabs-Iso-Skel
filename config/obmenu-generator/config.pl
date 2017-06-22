7be28a72dce7da269cbf0151dcb850f959623ea6
Erik Dubois <erik.dubois@gmail.com>
2017-06-22 10:28:07 +0200

    compared .config file with skel

--- config/obmenu-generator/config.pl
+++ config/obmenu-generator/config.pl
@@ -97,5 +97,5 @@ our $CONFIG = {
   "missing_icon"        => "gtk-missing-image",
   "name_keys"           => ["Name"],
   "terminal"            => "termite",
-  "VERSION"             => 0.7,
+  "VERSION"             => 0.71,
 }
